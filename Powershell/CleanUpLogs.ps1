95c60930525a2c28461a8c0520f037a4bbcc2c22
Haddad Pajum <70746896+hpajum@users.noreply.github.com>
2024-10-09 13:36:47 +0330

    Add files via upload

---
+++ Powershell/CleanUpLogs.ps1
@@ -0,0 +1,18 @@
+﻿$folderPath = "D:\Loging"
+$days = 1
+$currentDate = Get-Date
+$limit = $currentDate.AddDays(-$days)
+Get-ChildItem -Path $folderPath -Recurse | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force
+
+$folderPath = "C:\inetpub\logs\LogFiles"
+$days = 10
+$currentDate = Get-Date
+$limit = $currentDate.AddDays(-$days)
+Get-ChildItem -Path $folderPath -Recurse | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force
+
+
+$folderPath = "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\LOGS"
+$days = 5
+$currentDate = Get-Date
+$limit = $currentDate.AddDays(-$days)
+Get-ChildItem -Path $folderPath -Recurse | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Force
